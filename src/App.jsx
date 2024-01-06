313b6e3bb6e3b2b3f8b4bbe10ab783fdb87dff8e
bezicalboy <mhmdrcow3363@gmail.com>
2024-01-06 17:25:12 +0700

    ups

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,16 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
-  const [love, setLove] = useState('Rio gantenk site');
+  const [love, setLove] = useState('');
   const [quotes, setQuotes] = useState('');
   return (
     <>
+      <nav>Rio site</nav>
       <h1>{love}</h1>
       <h1>{quotes}</h1>
       <button
         onClick={() => {
-          setLove('BAPAK AKU ADIT');
+          setLove('love you dari komplek cibalok');
         }}
       >
         pencet aku dong
