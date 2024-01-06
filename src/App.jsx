92bfd5cc9d23f01a86046119e0e20f02fa2dd2e0
bezicalboy <mhmdrcow3363@gmail.com>
2024-01-06 17:22:34 +0700

    quotes

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,24 @@ import './App.css';
 
 function App() {
   const [love, setLove] = useState('Rio gantenk site');
+  const [quotes, setQuotes] = useState('');
   return (
     <>
       <h1>{love}</h1>
+      <h1>{quotes}</h1>
       <button
         onClick={() => {
           setLove('BAPAK AKU ADIT');
         }}
       >
         pencet aku dong
+      </button>
+      <button
+        onClick={() => {
+          setQuotes('jika kamu pusing tidurlah');
+        }}
+      >
+        Quotes hari ini
       </button>
     </>
   );
